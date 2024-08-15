5fd124116e510347e13d05719ba97477a807981b
Aleesssino <alesg3a1b4o1@gmail.com>
2024-08-15 22:39:37 +0200

    chore: Rename bids to bidsSchema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import { pgTable, serial } from "drizzle-orm/pg-core";
 
-export const bids = pgTable("bu_bids", {
+export const bidsSchema = pgTable("bu_bids", {
   id: serial("id").primaryKey(),
 });
